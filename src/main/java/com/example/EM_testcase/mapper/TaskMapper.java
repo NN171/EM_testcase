1c73e9ee4e9c06f36f32e0273f7272153cfbeba5
nn17 <yakor356@gmail.com>
2024-08-25 18:37:55 +0300

    feature: add mapper for task create and update

---
+++ src/main/java/com/example/EM_testcase/mapper/TaskMapper.java
@@ -0,0 +1,12 @@
+package com.example.EM_testcase.mapper;
+
+import com.example.EM_testcase.domain.Task;
+import com.example.EM_testcase.dto.CreateTaskDto;
+import com.example.EM_testcase.dto.UpdateTaskDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
+    Task createDtoToDomain(CreateTaskDto createTaskDto);
+    Task updateDtoToDomain(UpdateTaskDto updateTaskDto);
+}
--- src/main/java/com/example/EM_testcase/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/EM_testcase/service/impl/TaskServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.EM_testcase.service.impl;
 
 import com.example.EM_testcase.domain.Task;
 import com.example.EM_testcase.dto.CreateTaskDto;
+import com.example.EM_testcase.mapper.TaskMapper;
 import com.example.EM_testcase.model.repository.TaskRepository;
 import com.example.EM_testcase.service.TaskService;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskServiceImpl implements TaskService {
 
-    TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+    private final TaskRepository taskRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
     @Override
     public Task saveTask(CreateTaskDto createTask) {
-        return taskRepository.save(createTask);
+        Task newTask = taskMapper.createDtoToDomain(createTask);
+        return taskRepository.save(newTask);
     }
 }
