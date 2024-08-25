e471777bc8ca9590586c6c5f0bd839383241e900
nn17 <yakor356@gmail.com>
2024-08-25 17:23:38 +0300

    git init

---
+++ src/main/java/com/example/EM_testcase/EmTestcaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.EM_testcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmTestcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmTestcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EM_testcase/EmTestcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EM_testcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmTestcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
